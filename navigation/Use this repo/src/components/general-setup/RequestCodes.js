b155595676db2824558d54e272f9de84f935f5ce
sateesh jagan kanugula <sateez0608@gmail.com>
2016-08-24 14:08:51 +0530

    added navigation specs

---
+++ navigation/Use this repo/src/components/general-setup/RequestCodes.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+class RequestCodes extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  // Reset the sectionIndex after the component is rendered
+  componentDidMount() {
+    this.props.handleSectionSelect(0);
+  }
+
+  render() {
+    let sectionIndex = 0;
+    return (
+      <div>
+        <h5 id='sm5' role='navigation'>REQUEST CODES</h5>
+      </div>
+    )
+  }
+}
+
+export default RequestCodes;
