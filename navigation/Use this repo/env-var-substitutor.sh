4d2152db1db3aec2120436f8965c5505da52183e
sateesh jagan kanugula <sateez0608@gmail.com>
2016-08-30 14:47:46 +0530

    added two repos

--- navigation/Use this repo/env-var-substitutor.sh
+++
@@ -1,2 +0,0 @@
-# Replaces env specific placeholders using env variables
-sed -i -e "s|{{ENVIRONMENT}}|$ENVIRONMENT|g" -e "s|{{API_HOST}}|$API_HOST|g" -e "s|{{OAUTH_USERNAME}}|$OAUTH_USERNAME|g" -e "s|{{OAUTH_SECRET}}|$OAUTH_SECRET|g" assets/app.js
